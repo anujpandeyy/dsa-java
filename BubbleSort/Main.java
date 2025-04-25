a471480a0b38d54a482ec5dc83ea6635bbf2c835
Anuj Pandey <anujpandey1701@yahoo.com>
2025-04-25 09:59:34 +0530

    Bubble Sort

---
+++ BubbleSort/Main.java
@@ -0,0 +1,28 @@
+package BubbleSort;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = { 13, 46, 24, 52, 20, 9 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static int[] bubbleSort(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
