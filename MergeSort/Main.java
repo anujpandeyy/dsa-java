fa01fd91c6dfa3c6b2827ffdd6a03fc052468dc5
Anuj Pandey <anujpandey1701@yahoo.com>
2025-04-26 18:09:15 +0530

    Merge Sort

---
+++ MergeSort/Main.java
@@ -0,0 +1,62 @@
+package MergeSort;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = { 14, 9, 15, 12, 6, 8, 13 };
+        for (int i : nums) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        mergeSort(nums, 0, nums.length - 1);
+        for (int i : nums) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void mergeSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(nums, left, mid);
+            mergeSort(nums, mid + 1, right);
+            merge(nums, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] nums, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = nums[left + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = nums[mid + 1 + j];
+
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                nums[k] = L[i];
+                i++;
+            } else {
+                nums[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            nums[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            nums[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+}
